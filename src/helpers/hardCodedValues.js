d4c3cb311c7c7858bb0e8ec7b050c57f52b42dad
Claudio Tocco <tocco.a.claudio@gmail.com>
2019-10-12 20:48:50 +0200

    refactor to typescript

--- src/helpers/hardCodedValues.js
+++
@@ -1,9 +0,0 @@
-const Numbers = ["7", "8", "9", "4", "5", "6", "1", "2", "3", "0"];
-
-const Operators = ["/", "*", "-", "+", "="];
-
-const ClearString = "C";
-
-const DotString = ".";
-
-export { Numbers, Operators, ClearString, DotString };
